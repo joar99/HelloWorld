c640873374074ea8ebf2e116a9298becd9fdd5f1
theer <theereal_jonas@outlook.com>
2022-01-11 17:28:35 +0100

    Iterating string using map

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,5 +1,31 @@
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
+
+
+        Map myMap = new HashMap();
+
+
+        String str = "hello my name is jared";
+
+        for (char x:str.toCharArray()) {
+            if (myMap.containsKey(x)) {
+                int old = (int) myMap.get(x);
+                myMap.put(x, old+1);
+
+            }
+            else {
+                myMap.put(x, 1);
+            }
+        }
+
+        System.out.println(myMap);
+
+
     }
 }
