dfd6fb9ac3a18c95364fe729bc6830fb6f2a92d3
theer <theereal_jonas@outlook.com>
2022-01-11 17:44:36 +0100

    Iterating string using map

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -2,10 +2,17 @@ import java.util.HashMap;
 import java.util.HashSet;
 import java.util.LinkedHashMap;
 import java.util.Map;
+import java.util.Arrays;
 
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
+
+
+
+
+
+        //ITERATING AND COUNTING THROUGH STRING USING MAP
 
 
         Map myMap = new HashMap();
@@ -25,6 +32,21 @@ public class HelloWorld {
         }
 
         System.out.println(myMap);
+
+
+
+
+
+        //SORTING THROUGH AN ARRAY
+
+
+        int[] numArray = {99,15,17,22,35,8,4,2,1,6,7,213,124};
+
+        Arrays.sort(numArray);
+
+        for (int i = 0; i < numArray.length; i++) {
+            System.out.println(numArray[i]);
+        }
 
 
     }
